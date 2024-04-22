d2abbd4210aed2f3b1dfa84cd77256ea94fd6030
gopikrishnatgk <57552463+gopikrishnatgk@users.noreply.github.com>
2024-04-22 08:40:59 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import statistics
+from scipy.stats import mode
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
